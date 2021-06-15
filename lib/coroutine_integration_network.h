63c70f10abab1b329eaa5d0e278fc8d2bc1617fd
Jan Blackquill <uhhadd@gmail.com>
2021-06-15 18:29:56 -0400

    chore: remove unused code

--- lib/coroutine_integration_network.h
+++ lib/coroutine_integration_network.h
@@ -25,24 +25,3 @@ struct transformer<QNetworkReply*>
         return future;
     }
 };
-
-// auto operator co_await(QNetworkReply* it) noexcept
-// {
-//     struct Awaiter {
-//         FutureBase future;
-
-//         bool await_ready() const noexcept {
-//             return future.settled();
-//         }
-//         void await_suspend(ns::coroutine_handle<> cont) const {
-//             future.then([cont](QVariant) mutable {
-//                 cont();
-//             });
-//         }
-//         QVariant await_resume() {
-//             return future.result();
-//         }
-//     };
-
-//     return Awaiter{ it };
-// }
